bef7ff80c1a702729bfb045369fb70deeed54823
Daniel Krueger <krugerdaniel2000@gmail.com>
2025-05-29 17:55:29 +0900

    Fix imports

--- scripts/math_graph_importer.py
+++ scripts/math_graph_importer.py
@@ -5,8 +5,8 @@ import time
 
 from datasets import load_dataset
 
-from scripts.config import PY_KEYWORDS
-from scripts.filters.library_filters import filter_example
+from config import PY_KEYWORDS
+from filters.library_filters import filter_example
 
 library_type = sys.argv[1] if len(sys.argv) > 1 else None
 if not library_type or library_type not in PY_KEYWORDS.keys():
